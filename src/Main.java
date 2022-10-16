16098222583ae84f2f06cea5ad9eb2141a55b9ae
DenisKrinitsyn <skylinekrin@gmail.com>
2022-10-16 22:09:41 +0500

    Add Task2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     static Scanner scanner = new Scanner(System.in);
 
-    /** A program that guesses a random number from 0 to 9 and the user is given 3 attempts to guess!
-     *
+    /**
+     * A program that guesses a random number from 0 to 9 and the user is given 3 attempts to guess!
      */
     public static void task1() {
         int random = (int) (Math.random() * 10);
@@ -31,21 +32,68 @@ public class Main {
                 System.out.println("Вы ввели некорректное число. Повторите попытку ввода.");
                 scanner.nextLine();
             }
-        } while (counter !=3);
+        } while (counter != 3);
         System.out.println("Вы не угадали, но Вы всегда можете попробовать снова\n");
 
     }
 
     public static void task2() {
-        System.out.println("Process task2...");
+        String[] words = {"apple"
+//                , "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
+//                "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea",
+//                "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"
+        };
+
+        char[] answer = {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'};
+
+
+        Random rand = new Random();
+        int index = rand.nextInt(words.length);
+        String str = words[index];
+        int i = 0;
+        System.out.println(words[index]);
+
+        System.out.println("Робот загадал слово из этого спика:");
+        System.out.println("\"apple\", \"orange\", \"lemon\", \"banana\", \"apricot\", \"avocado\", \"broccoli\", \"carrot\",\n" +
+                "                \"cherry\", \"garlic\", \"grape\", \"melon\", \"leak\", \"kiwi\", \"mango\", \"mushroom\", \"nut\", \"olive\", \"pea\",\n" +
+                "                \"peanut\", \"pear\", \"pepper\", \"pineapple\", \"pumpkin\", \"potato\"");
+        System.out.println("Попробуйте угадать его!");
+
+        String enter;
+
+        do {
+            enter = scanner.nextLine();
+            //char[] ent = {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'};
+//            for(int j = 0; j < enter.length(); j++){
+//                System.out.println(enter.length());
+//
+//                ent[j] = enter.charAt(j);
+//            }
+            //char ent = enter.charAt(i);
+            //char a = str.charAt(i);
+            for (int j = 0; j < enter.length(); j++) {
+                if (enter.charAt(j) == str.charAt(j)) {
+                    answer[j] = enter.charAt(j);
+                    System.out.print(answer[j]);
+                }
+
+                //System.out.print(answer[i]);
+            }
+
+            i++;
+            if (enter == words[index]) {
+                System.out.println("Вы угадали слово!");
+            }
+        } while (enter != words[index]);
     }
 
     public static void main(String[] args) {
 
         boolean f = true;
         while (f) {
+            System.out.println("Выберите вариент:");
             System.out.println("1 - Угадай число от 0 до 9");
-            System.out.println("2 - Task2");
+            System.out.println("2 - Учитель, дайте совет, как мне решить Task2. Сам не смог");
             System.out.print("Введите номер задачи (0 - выход из программы): ");
 
             if (scanner.hasNextInt()) {
